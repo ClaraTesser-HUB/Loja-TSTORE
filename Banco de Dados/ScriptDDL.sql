5eb172fe172f668acf8688cda5132cfcc9320062
ClaraTesser-HUB <claratesserpereira@gmail.com>
2025-02-10 16:44:45 -0300

    Template

---
+++ Banco de Dados/ScriptDDL.sql
@@ -0,0 +1,32 @@
+-- Criar um Banco
+CREATE DATABASE lojinha;
+use lojinha;
+CREATE TABLE Categoria
+(
+	Id			int not null identity primary key,
+	Nome		varchar(50),
+	Foto		varchar(200),
+	ExibirHome	bit
+);
+
+CREATE TABLE Produto
+(
+	Id			int not null identity primary key,
+	Nome		varchar (100) not null,
+	Descricao	varchar (1000),
+	Categoria	int not null,
+	ValorCusto	numeric(10,2),
+	ValorVenda	numeric(10,2) not null,
+	QtdeEstoque	int not null default 0,
+	Destaque	bit default 0,
+	foreign key (CategoriaId) references Categoria(Id)
+);
+
+CREATE TABLE ProdutoFoto
+(
+	Id			int not null identity primary key,
+	ProdutoId	int not null,
+	Foto		varchar(200),
+	Descricao	varchar(100)
+	foreign key	(ProdutoId) references Produto(Id),
+);
